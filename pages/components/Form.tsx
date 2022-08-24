a243d45a1f2fcd42af6aa2afc3127a46ece64762
‘terupro’ <terupro911@gmail.com>
2022-08-24 19:30:57 +0900

    ✨ 追加・削除・更新

---
+++ pages/components/Form.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+type FormProps = {
+  createTodo: Function;
+};
+
+const Form: React.FC<FormProps> = ({ createTodo }) => {
+  const [enteredTodo, setEnteredTodo] = useState("");
+
+  const addTodo = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const newTodo = {
+      id: Math.floor(Math.random() * 1e5),
+      content: enteredTodo,
+    };
+
+    createTodo(newTodo);
+
+    setEnteredTodo("");
+  };
+  return (
+    <div>
+      <form onSubmit={addTodo}>
+        <input
+          type="text"
+          value={enteredTodo}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setEnteredTodo(e.target.value)
+          }
+        />
+        <button>追加</button>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
